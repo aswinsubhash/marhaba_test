2b45d7c0af073a66faedfc77da91c452c375394e
Aswin Subhash <aswinsubhash@Mac.lan>
2026-02-19 21:14:04 +0400

    feat: Add copyWith method and remove SafeArea
    - Add copyWith method to VideoEntity base class
    - Override copyWith in VideoModel to return correct type
    - Remove SafeArea from VideoReelsPage

--- lib/features/video/domain/entities/video_entity.dart
+++ lib/features/video/domain/entities/video_entity.dart
@@ -15,6 +15,22 @@ class VideoEntity extends Equatable {
     required this.author,
   });
 
+  VideoEntity copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? videoUrl,
+    String? author,
+  }) {
+    return VideoEntity(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      videoUrl: videoUrl ?? this.videoUrl,
+      author: author ?? this.author,
+    );
+  }
+
   @override
   List<Object?> get props => [id, title, description, videoUrl, author];
 }
